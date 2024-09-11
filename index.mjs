970f6f22c3f9e199cd6e7826a5fef5e12ca000d4
Aiden Cullo <aidencullo@gmail.com>
2024-09-11 08:14:14 -1000

    list collection validator

--- index.mjs
+++ index.mjs
@@ -7,74 +7,11 @@ const app = express();
 
 app.use(express.json());
 
-// Create Collection with Schema Validation
-// We're wrapping ours in a function to prevent it
-// from being called again, since we've already
-// created this collection.
-async () => {
-  await db.command({
-    collMod: "learners",
-    // Pass the validator object
-    validator: {
-      // Use the $jsonSchema operator
-      $jsonSchema: {
-        bsonType: "object",
-        title: "Learner Validation",
-        // List required fields
-        required: ["name", "enrolled", "year", "campus"],
-        // Properties object contains document fields
-        properties: {
-          name: {
-            // Each document field is given validation criteria
-            bsonType: "string",
-            // and a description that is shown when a document fails validation
-            description: "'name' is required, and must be a string",
-          },
-          enrolled: {
-            bsonType: "bool",
-            description: "'enrolled' status is required and must be a boolean",
-          },
-          year: {
-            bsonType: "int",
-            minimum: 1995,
-            description:
-              "'year' is required and must be an integer greater than 1995",
-          },
-          avg: {
-            bsonType: "double",
-            description: "'avg' must be a double",
-          },
-          campus: {
-            enum: [
-              "Remote",
-              "Boston",
-              "New York",
-              "Denver",
-              "Los Angeles",
-              "Seattle",
-              "Dallas",
-            ],
-            description: "Invalid campus location",
-          },
-        },
-      },
-    },
-  });
-};
-
-// Test the schema validation by inserting an invalid document
+// Get the current validation rules.
 app.get("/", async (req, res) => {
-  let collection = await db.collection("learners");
-  let newDocument = {
-    name: "Frodo",
-    enrolled: true,
-    year: 2024,
-    campus: "The Shire",
-  };
+  let coll = await db.listCollections({ name: "learners" }).toArray();
+  const result = coll[0].options.validator;
 
-  let result = await collection.insertOne(newDocument).catch((e) => {
-    return e.errInfo.details.schemaRulesNotSatisfied;
-  });
   res.send(result).status(204);
 });
 
